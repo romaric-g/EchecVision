f2892479c271b86d09d3d7c3e28c133278641859
romaric-g <romaric.gauzi1@gmail.com>
2023-04-24 00:36:09 +0200

    clean

--- echec_vision/core/server/connector.py
+++ echec_vision/core/server/connector.py
@@ -51,7 +51,7 @@ class Connector:
             draw_moov_parse = [chess.square_name(
                 draw_moov.from_square), chess.square_name(draw_moov.to_square)]
 
-        if session is not None:
+        if session is not None and session.is_start:
             chess_board_state: ChessBoardState = ChessBoardState(
                 fen=session.move_detector.game.board.fen(),
                 draw_moov=draw_moov_parse
@@ -59,7 +59,7 @@ class Connector:
 
             self.chess_board_state_dumps = json.dumps(chess_board_state)
             self.get_socketio().emit("new_chess_board_state", self.chess_board_state_dumps)
-        
+
     def get_session(self):
         from core.session import session
         return session
